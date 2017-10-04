028f4d0d099d09ddf1f69ebd2bc53d2ae0a535b9
Mike Kuiken <mkuiken@restaurant.org>
2017-10-04 11:22:44 -0500

    added DbContext.Eagle & Customer

---
+++ BeechTree/DAL/DbContext.Eagle.cs
@@ -0,0 +1,32 @@
+﻿using BeechTree.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BeechTree.DAL
+{
+    public class DbContext_Eagle : DbContext
+    {        
+        public DbContext_Eagle()
+            : base("Eagle")
+        {
+            Database.SetInitializer<InvoiceDBContext>
+                (null);
+        }
+        
+        public DbSet<Customer> Customers { get; set; }
+
+        public List<Customer> CustomersGet(string companyNumber)
+        {
+            var records = this.Customers
+                        .Where(x => x.CompanyNumber.Equals(companyNumber))
+                        .OrderBy(x => x.CompanyName)
+                        .ToList();
+
+            return records;
+        }
+
+
+    }
+}
