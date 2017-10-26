6ad508a305149a2389f459fefddd806c109bf6e7
Mike K <mikek@uniek.net>
2017-10-26 10:36:03 -0500

    add customer UI

---
+++ BeechTree/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using BeechTree.DAL;
+using BeechTree.Models;
+using Novacode;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+
+namespace BeechTree.Controllers
+{
+	public class CustomerController : BaseController
+    {
+        private DbContext_Eagle dbEagle = new DbContext_Eagle();
+
+        public ActionResult Index(string filter = null, int page = 1, int pageSize = 5, string sort = "CustName", string sortdir = "ASC")
+        {
+            var records = new PagedList<Customer>();
+            ViewBag.filter = filter;
+            records.Content = dbEagle.Customers
+                        .Where(x => filter == null ||
+                                (x.CustId.Contains(filter))
+                                   || x.CustName.Contains(filter)
+                              )
+                        .OrderBy(sort + " " + sortdir)
+                        .Skip((page - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToList();
+
+            // Count
+            records.TotalRecords = dbEagle.Customers
+                         .Where(x => filter == null ||
+                               (x.CustId.Contains(filter)) || x.CustName.Contains(filter)).Count();
+
+            records.CurrentPage = page;
+            records.PageSize = pageSize;
+
+            return View(records);
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            dbEagle.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
